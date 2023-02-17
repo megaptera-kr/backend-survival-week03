18b5bed9e4b1684fa11a1a111b1aa190134d46c7
gugbab2 <jkhoo1997@gmail.com>
2023-02-18 01:04:09 +0900

    게시물, 댓글 API

--- src/main/java/kr/megaptera/assignment/controllers/CommentController.java
+++ src/main/java/kr/megaptera/assignment/controllers/CommentController.java
@@ -1,12 +1,84 @@
 package kr.megaptera.assignment.controllers;
 
 import kr.megaptera.assignment.dtos.CommentDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@RestController
+@RequestMapping("/comments")
+@CrossOrigin("http://localhost:8000")
 public class CommentController {
     private Long newId = 0L;
 
     private List<CommentDto> commentDtos = new ArrayList<>();
+
+    @GetMapping
+    public List<CommentDto> list(
+            @RequestParam String postId
+    ) {
+        List<CommentDto> tempDtos = new ArrayList<>();
+        for (int i = 0; i < commentDtos.size(); i++) {
+            CommentDto commentDto = commentDtos.get(i);
+
+            if (commentDto.getPostId().equals(postId))
+                tempDtos.add(commentDto);
+        }
+        return tempDtos;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String create(
+            @RequestParam String postId,
+            @RequestBody CommentDto body
+    ) {
+        Long id = autoIncremenet();
+        body.setId(String.valueOf(id));
+        body.setPostId(postId);
+
+        commentDtos.add(body);
+
+        return "complate!";
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update(
+            @PathVariable String id,
+            @RequestParam String postId,
+            @RequestBody CommentDto body
+    ) {
+        for (int i = 0; i < commentDtos.size(); i++) {
+            CommentDto commentDto = commentDtos.get(i);
+            if (commentDto.getPostId().equals(postId)) {
+                if (commentDto.getId().equals(id)) {
+                    commentDto.setId(body.getId());
+                    commentDto.setContent(body.getContent());
+                }
+            }
+        }
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(
+            @PathVariable String id,
+            @RequestParam String postId
+    ) {
+        for (int i = 0; i < commentDtos.size(); i++) {
+            CommentDto commentDto = commentDtos.get(i);
+            if (commentDto.getPostId().equals(postId)) {
+                if (commentDto.getId().equals(id)) {
+                    commentDtos.remove(i);
+                }
+            }
+        }
+    }
+
+    private Long autoIncremenet() {
+        return ++newId;
+    }
 }
--- src/main/java/kr/megaptera/assignment/controllers/PostController.java
+++ src/main/java/kr/megaptera/assignment/controllers/PostController.java
@@ -1,12 +1,80 @@
 package kr.megaptera.assignment.controllers;
 
 import kr.megaptera.assignment.dtos.PostDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@RestController
+@RequestMapping("/posts")
+@CrossOrigin("http://localhost:8000")
 public class PostController {
     private Long newId = 0L;
 
     private List<PostDto> postDtos = new ArrayList<>();
+
+    @GetMapping
+    public List<PostDto> list() {
+        return postDtos;
+    }
+
+    @GetMapping("{id}")
+    public PostDto detail(
+            @PathVariable String id
+    ) {
+        for (int i = 0; i < postDtos.size(); i++) {
+            PostDto postDto = postDtos.get(i);
+            if (postDto.getId().equals(id))
+                return postDto;
+        }
+        return null;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String create(
+            @RequestBody PostDto body
+    ) {
+        Long id = autoIncremenet();
+        body.setId(String.valueOf(id));
+
+        postDtos.add(body);
+
+        return "Complate!";
+    }
+
+    @PutMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update(
+            @PathVariable String id,
+            @RequestBody PostDto body
+    ) {
+        for (int i = 0; i < postDtos.size(); i++) {
+            PostDto postDto = postDtos.get(i);
+            if (postDto.getId().equals(id)) {
+                postDto.setId(body.getId());
+                postDto.setTitle(body.getTitle());
+                postDto.setContent(body.getContent());
+            }
+        }
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(
+            @PathVariable String id
+    ) {
+        for (int i = 0; i < postDtos.size(); i++) {
+            PostDto postDto = postDtos.get(i);
+            if (postDto.getId().equals(id)) {
+                postDtos.remove(i);
+            }
+        }
+    }
+
+    private Long autoIncremenet() {
+        return ++newId;
+    }
 }
