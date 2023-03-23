a4c674ad9bbcd2bf005218b2d97e3e7ad7b0b000
byeon.sungmun <201401339hs@gmail.com>
2023-03-24 01:23:42 +0900

    feat : 과제 제출

--- src/main/java/kr/megaptera/assignment/dtos/CommentDto.java
+++ src/main/java/kr/megaptera/assignment/dtos/CommentDto.java
@@ -1,4 +1,41 @@
 package kr.megaptera.assignment.dtos;
 
+
 public class CommentDto {
+    private String id;
+    private String postId;
+    private String content;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        if (id == null || id.isEmpty()) {
+            new IllegalArgumentException("댓글 ID가 없습니다..");
+        }
+        this.id = id;
+    }
+
+    public String getPostId() {
+        return postId;
+    }
+
+    public void setPostId(String postId) {
+        if (postId == null || postId.isEmpty()) {
+            new IllegalArgumentException("게시물 ID가 없습니다..");
+        }
+        this.postId = postId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        if (content == null || content.isEmpty()) {
+            new IllegalArgumentException("내용이 없습니다.");
+        }
+        this.content = content;
+    }
 }
