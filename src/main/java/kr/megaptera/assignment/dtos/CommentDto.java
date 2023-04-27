be33af9758ae7301264c672045c6e68a6e9e80bc
spacekay <bethjkim0311@gmail.com>
2023-04-28 00:02:08 +0900

    [Week 3] feat: PostDTO, CommentDTO 정의

--- src/main/java/kr/megaptera/assignment/dtos/CommentDto.java
+++ src/main/java/kr/megaptera/assignment/dtos/CommentDto.java
@@ -1,4 +1,43 @@
 package kr.megaptera.assignment.dtos;
 
 public class CommentDto {
+
+    private String id;
+
+    private String postId;
+
+    private String content;
+
+    public CommentDto() {
+    }
+
+    public CommentDto(String id, String postId, String content) {
+        this.id = id;
+        this.postId = postId;
+        this.content = content;
+    }
+
+    public CommentDto(String content) {
+        this.content = content;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getPostId() {
+        return this.postId;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+
+    public void setId(Long id) {
+        this.id = id.toString();
+    }
+
+    public void setPostId(String postId) {
+        this.postId = postId;
+    }
 }
--- src/main/java/kr/megaptera/assignment/dtos/PostDto.java
+++ src/main/java/kr/megaptera/assignment/dtos/PostDto.java
@@ -1,4 +1,41 @@
 package kr.megaptera.assignment.dtos;
 
 public class PostDto {
+
+    private String id;
+
+    private String title;
+
+    private String content;
+
+    // Default Constructor가 없으면 RequestBody를 DTO로 받기가 자동으로 안됨
+    public PostDto() {
+    }
+
+    public PostDto(String id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    public PostDto(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+
+    public void setId(Long id) {
+        this.id = id.toString();
+    }
 }
