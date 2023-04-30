b153e57d31af2d24b115693fdd618b34eb07f31f
Ohjinn <hjin9445@gmail.com>
2023-04-30 19:51:42 +0900

    change_error_log_policy

--- src/main/java/kr/megaptera/assignment/AssignmentApplication.java
+++ src/main/java/kr/megaptera/assignment/AssignmentApplication.java
@@ -3,6 +3,8 @@ package kr.megaptera.assignment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -23,5 +25,21 @@ public class AssignmentApplication {
 						.allowedOrigins("http://localhost:8000");
 			}
 		};
+	}
+
+	@Configuration
+	public class RequestLoggingFilterConfig {
+
+		@Bean
+		public CommonsRequestLoggingFilter logFilter() {
+			CommonsRequestLoggingFilter filter
+					= new CommonsRequestLoggingFilter();
+			filter.setIncludeQueryString(true);
+			filter.setIncludePayload(true);
+			filter.setMaxPayloadLength(10000);
+			filter.setIncludeHeaders(false);
+			filter.setAfterMessagePrefix("REQUEST DATA: ");
+			return filter;
+		}
 	}
 }
--- src/main/java/kr/megaptera/assignment/controllers/CommentController.java
+++ src/main/java/kr/megaptera/assignment/controllers/CommentController.java
@@ -1,12 +1,18 @@
 package kr.megaptera.assignment.controllers;
 
 import kr.megaptera.assignment.dtos.CommentDto;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@RestController
+@RequestMapping("comments")
 public class CommentController {
     private Long newId = 0L;
 
     private List<CommentDto> commentDtos = new ArrayList<>();
+
+
 }
