b532980f7e5e74415057c92a1709044f1ffc0ac6
p-ej <show400035@naver.com>
2023-04-30 17:49:50 +0900

    feat: 댓글 불러오기 구현

--- src/main/java/kr/megaptera/assignment/controllers/CommentController.java
+++ src/main/java/kr/megaptera/assignment/controllers/CommentController.java
@@ -1,12 +1,29 @@
 package kr.megaptera.assignment.controllers;
 
 import kr.megaptera.assignment.dtos.CommentDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@RestController
+@RequestMapping("/comments")
 public class CommentController {
     private Long newId = 0L;
 
     private List<CommentDto> commentDtos = new ArrayList<>();
+
+
+    @GetMapping
+    private List<CommentDto> getComments(@RequestParam("postId") String postId) {
+        List<CommentDto> findComments = commentDtos
+                .stream()
+                .filter(comment -> comment.getPostId().equals(postId))
+                .toList();
+        return findComments;
+    }
+
 }
