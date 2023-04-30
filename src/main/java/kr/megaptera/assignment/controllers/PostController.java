0d4d6cae267414b5d90ea7c2965b3fd811ea3942
p-ej <show400035@naver.com>
2023-04-30 16:38:29 +0900

    feat: 게시물 리스트 불러오기 핸들러 구현

--- src/main/java/kr/megaptera/assignment/controllers/PostController.java
+++ src/main/java/kr/megaptera/assignment/controllers/PostController.java
@@ -1,12 +1,22 @@
 package kr.megaptera.assignment.controllers;
 
 import kr.megaptera.assignment.dtos.PostDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@RestController
+@RequestMapping("/posts")
 public class PostController {
     private Long newId = 0L;
 
     private List<PostDto> postDtos = new ArrayList<>();
+
+    @GetMapping
+    private List<PostDto> getPosts() {
+        return postDtos;
+    }
 }
