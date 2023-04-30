71511f010055c7ce1464d4aa2737b11bd4c82689
p-ej <show400035@naver.com>
2023-04-30 16:47:18 +0900

    feat: 게시물 불러오기 핸들러 구현

--- src/main/java/kr/megaptera/assignment/controllers/PostController.java
+++ src/main/java/kr/megaptera/assignment/controllers/PostController.java
@@ -2,11 +2,13 @@ package kr.megaptera.assignment.controllers;
 
 import kr.megaptera.assignment.dtos.PostDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/posts")
@@ -19,4 +21,13 @@ public class PostController {
     private List<PostDto> getPosts() {
         return postDtos;
     }
+
+    @GetMapping("/{id}")
+    private Optional<PostDto> getPost(@PathVariable("id") String id) {
+        Optional<PostDto> findPost = postDtos.stream()
+                .filter(post -> post.getId().equals(id))
+                .findFirst();
+        return findPost;
+    }
+
 }
