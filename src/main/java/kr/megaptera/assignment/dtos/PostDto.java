9b550ccbf0f6f0ef358afa54265d61dc501174c8
Ohjinn <hjin9445@gmail.com>
2023-04-30 19:31:43 +0900

    post_complete

--- src/main/java/kr/megaptera/assignment/dtos/PostDto.java
+++ src/main/java/kr/megaptera/assignment/dtos/PostDto.java
@@ -1,4 +1,49 @@
 package kr.megaptera.assignment.dtos;
 
 public class PostDto {
+    private String id;
+    private String title;
+    private String content;
+
+    public PostDto() {
+    }
+
+    public PostDto(String id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "PostDto{" +
+                "id='" + id + '\'' +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
 }
