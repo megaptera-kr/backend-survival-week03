7bbc4592e0bcf5ad0c7cbe16bf8852d9166a1b2c
immsee098 <immsee098@gmail.com>
2023-05-01 12:17:57 +0900

    3주차 과제 제출

--- src/main/java/kr/megaptera/assignment/controllers/CommentController.java
+++ src/main/java/kr/megaptera/assignment/controllers/CommentController.java
@@ -1,12 +1,72 @@
 package kr.megaptera.assignment.controllers;
 
-import kr.megaptera.assignment.dtos.CommentDto;
+import kr.megaptera.assignment.dtos.*;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.*;
 
+@RestController
+@RequestMapping("/posts")
 public class CommentController {
     private Long newId = 0L;
 
     private List<CommentDto> commentDtos = new ArrayList<>();
+
+    private String makeId() {
+        newId += 1;
+        return newId.toString();
+    }
+
+    @GetMapping
+    public List<CommentDto> list(@RequestParam String postId) {
+        List<CommentDto> dtoList = commentDtos.stream()
+                .filter(i -> i.getPostId().equals(postId))
+                .toList();
+
+        return dtoList;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String create(
+            @RequestParam String postId,
+            @RequestBody CommentDto commentDto
+    ) {
+        commentDto.setId(makeId());
+        commentDto.setPostId(postId);
+
+        commentDtos.add(commentDto);
+
+        return "Complete!";
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update(
+            @PathVariable String id,
+            @RequestParam String postId,
+            @RequestBody CommentDto commentDto
+    ) {
+        commentDtos = commentDtos.stream()
+                .map(item -> item.getPostId().equals(postId) && item.getId().equals(id)
+                        ? commentDto
+                        : item)
+                .collect(Collectors.toList());
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(
+            @RequestParam String postId,
+            @PathVariable String id
+    ) {
+        CommentDto commentDto = commentDtos.stream()
+                .filter(item -> item.getPostId().equals(postId) && item.getId().equals(id))
+                .findFirst()
+                .get();
+
+        commentDtos.remove(commentDto);
+    }
 }
--- src/main/java/kr/megaptera/assignment/dtos/CommentDto.java
+++ src/main/java/kr/megaptera/assignment/dtos/CommentDto.java
@@ -1,4 +1,42 @@
 package kr.megaptera.assignment.dtos;
 
 public class CommentDto {
+    private String id;
+
+    private String postId;
+
+    private String content;
+
+    public CommentDto() {
+    }
+
+    public CommentDto(String id, String postId, String content) {
+        this.id = id;
+        this.postId = postId;
+        this.content = content;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPostId() {
+        return postId;
+    }
+
+    public void setPostId(String postId) {
+        this.postId = postId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
