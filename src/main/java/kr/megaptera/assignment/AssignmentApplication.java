9ca5790d1698fba0c99fc00efd50990254d20e56
immsee098 <immsee098@gmail.com>
2023-05-01 13:48:47 +0900

    3주차 과제 제출2

--- src/main/java/kr/megaptera/assignment/AssignmentApplication.java
+++ src/main/java/kr/megaptera/assignment/AssignmentApplication.java
@@ -1,13 +1,27 @@
 package kr.megaptera.assignment;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.context.annotation.*;
+import org.springframework.web.servlet.config.annotation.*;
 
 @SpringBootApplication
 public class AssignmentApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AssignmentApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AssignmentApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry
+                        .addMapping("/**")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+            }
+        };
+    }
 
 }
