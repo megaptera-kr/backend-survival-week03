d7e2157eff1daeb622625de8a9760217e6890289
LCH <ckcks1997@gmail.com>
2023-05-01 13:42:24 +0900

    3ㅈㅜ차 과제

--- src/main/java/kr/megaptera/assignment/dtos/PostDto.java
+++ src/main/java/kr/megaptera/assignment/dtos/PostDto.java
@@ -1,4 +1,43 @@
 package kr.megaptera.assignment.dtos;
 
+
 public class PostDto {
+
+    private String id;
+    private String title;
+    private String content;
+
+
+    public PostDto() {
+    }
+
+    public PostDto(String id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String gettitle() {
+        return title;
+    }
+
+    public void settitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
