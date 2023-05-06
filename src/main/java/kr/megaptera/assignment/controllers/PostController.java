ae8f246fce2a1724f61bb532b0b5f2c5b53d1f5a
DYKIM\kdy95 <kdy950930@gmail.com>
2023-05-07 02:51:09 +0900

    GET 까지만

--- src/main/java/kr/megaptera/assignment/controllers/PostController.java
+++ src/main/java/kr/megaptera/assignment/controllers/PostController.java
@@ -1,12 +1,31 @@
 package kr.megaptera.assignment.controllers;
 
 import kr.megaptera.assignment.dtos.PostDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@CrossOrigin
+@RestController
+@RequestMapping("/posts")
 public class PostController {
     private Long newId = 0L;
 
-    private List<PostDto> postDtos = new ArrayList<>();
+    private List<PostDto> postDtos = new ArrayList<>(List.of(
+            new PostDto("1","제목1", "내용1")
+        )
+    );
+
+    @GetMapping
+    public List<PostDto> list(){
+        return postDtos;
+    }
+    @GetMapping("/{id}")
+    public PostDto detail(@PathVariable String id){
+
+        return postDtos.get(Integer.parseInt(id) - 1);
+    }
+
 }
--- src/main/java/kr/megaptera/assignment/dtos/PostDto.java
+++ src/main/java/kr/megaptera/assignment/dtos/PostDto.java
@@ -1,4 +1,49 @@
 package kr.megaptera.assignment.dtos;
 
 public class PostDto {
+    /**
+     * 게시물 ID
+     */
+    private String id;
+    /**
+     * 게시물 제목
+     */
+    private String title;
+    /**
+     * 게시물 내용
+     */
+    private String content;
+
+    public PostDto() {
+    }
+
+    public PostDto(String id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 }
