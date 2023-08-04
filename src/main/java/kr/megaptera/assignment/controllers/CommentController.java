83419d2bd6f9681f34d299d77bdb157410505501
sws6641 <sws6641@naver.com>
2023-08-04 17:58:07 +0900

    3주차 과제 제출

--- src/main/java/kr/megaptera/assignment/controllers/CommentController.java
+++ src/main/java/kr/megaptera/assignment/controllers/CommentController.java
@@ -1,12 +1,86 @@
 package kr.megaptera.assignment.controllers;
 
 import kr.megaptera.assignment.dtos.CommentDto;
+import kr.megaptera.assignment.dtos.PostDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
+@RestController
+@RequestMapping("/comments")
 public class CommentController {
     private Long newId = 0L;
 
     private List<CommentDto> commentDtos = new ArrayList<>();
+
+
+    @GetMapping
+    public List<CommentDto> list(@RequestParam String postId){
+
+        List<CommentDto> commentDtoList = commentDtos.stream()
+                .filter(i -> i.getPostId().equals(postId))
+                .toList();
+
+        return commentDtoList;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String create(
+            @RequestParam String postId,
+            @RequestBody CommentDto commentDto
+    ){
+        commentDto.setId(increaseId());
+        commentDto.setPostId(postId);
+        commentDto.setContent(commentDto.getContent());
+
+        commentDtos.add(commentDto);
+
+        return "Complete!";
+    }
+
+    @PutMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update(
+            @PathVariable String id,
+            @RequestParam String postId,
+            @RequestBody CommentDto commentDto
+    ){
+        commentDtos = commentDtos.stream()
+                .map(i -> i.getId().equals(id) && i.getPostId().equals(postId) ? commentDto : i)
+                .collect(Collectors.toList());
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(
+            @PathVariable String id,
+            @RequestParam String postId
+    ){
+        CommentDto commentDto = commentDtos.stream()
+                .filter(i -> i.getId().equals(id) && i.getPostId().equals(postId))
+                .findFirst().get();
+
+        commentDtos.remove(commentDto);
+    }
+
+    // 댓글의 아이디 값 1 씩 증가
+    public String increaseId(){
+        newId++;
+        return newId.toString();
+    }
+
+
 }
--- src/main/java/kr/megaptera/assignment/controllers/PostController.java
+++ src/main/java/kr/megaptera/assignment/controllers/PostController.java
@@ -1,12 +1,79 @@
 package kr.megaptera.assignment.controllers;
 
 import kr.megaptera.assignment.dtos.PostDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
+@RestController
+@RequestMapping("/posts")
 public class PostController {
     private Long newId = 0L;
 
     private List<PostDto> postDtos = new ArrayList<>();
+
+    @GetMapping()
+    public List<PostDto> list(){
+        return postDtos;
+    }
+
+    @GetMapping("/{id}")
+    public PostDto detail(@PathVariable String id){
+        PostDto a = postDtos.stream()
+                .filter(i -> i.getId().equals(id))
+                .findFirst().get();
+
+        PostDto postDto = new PostDto(a.getId(), a.getTitle(), a.getContent());
+
+        return postDto;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String create(@RequestBody PostDto postDto){
+        postDto.setId(increaseId());
+
+        postDtos.add(postDto);
+
+        return "Complete!";
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update(@PathVariable String id, @RequestBody PostDto postDto){
+
+        postDtos = postDtos.stream()
+                .map(i -> i.getId().equals(id) ? postDto : i)
+                .collect(Collectors.toList());
+
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable String id){
+        PostDto postDto = postDtos.stream()
+                .filter(i -> i.getId().equals(id))
+                .findFirst().get();
+
+        postDtos.remove(postDto);
+
+    }
+
+    // 게시물의 아이디 값 1 씩 증가
+    public String increaseId(){
+        newId++;
+        return newId.toString();
+    }
+
 }
