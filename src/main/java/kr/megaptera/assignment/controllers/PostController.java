f485ad8e75a480c2896cbcddd39bd566d176383f
honki12345 <iui4793@gmail.com>
2023-08-05 23:05:55 +0900

    Feat #1 - PostController 생성과 게시글 생성 api 구현
    PostController 를 생성했다.
    통합테스트시 데이터베이스 쿼리 로그가 잘 뜨는지 확인하기 위해 게시글 생성 메서드를 간단하게 구현했다.

--- src/main/java/kr/megaptera/assignment/controllers/PostController.java
+++ src/main/java/kr/megaptera/assignment/controllers/PostController.java
@@ -1,12 +1,34 @@
 package kr.megaptera.assignment.controllers;
 
-import kr.megaptera.assignment.dtos.PostDto;
+import kr.megaptera.assignment.dtos.PostDTO;
+import kr.megaptera.assignment.repository.PostRepository;
+import kr.megaptera.assignment.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@RestController
+@RequestMapping("/posts")
+@RequiredArgsConstructor
 public class PostController {
+    public static final String SUCCESS_MESSAGE = "Complete!";
+    private final PostService postService;
+
+    // TODO 삭제
     private Long newId = 0L;
 
-    private List<PostDto> postDtos = new ArrayList<>();
+    // TODO 삭제
+    private List<PostDTO> postDtos = new ArrayList<>();
+
+    @PostMapping
+    public String create(@RequestBody PostDTO postDTO) {
+        postService.create(postDTO);
+
+        return SUCCESS_MESSAGE;
+    }
 }
