1abc5bb2d120d57d276fc87e9bfc46c4270b1cf2
honki12345 <iui4793@gmail.com>
2023-08-05 23:07:38 +0900

    Feat #3 - 댓글 엔터티 객체 생성

---
+++ src/main/java/kr/megaptera/assignment/domain/Comment.java
@@ -0,0 +1,41 @@
+package kr.megaptera.assignment.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Objects;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String content;
+
+    @ManyToOne
+    private Post post;
+
+    @Builder
+    private Comment(String content, Post post) {
+        this.content = content;
+        this.post = post;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Comment comment)) return false;
+        return Objects.equals(id, comment.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
