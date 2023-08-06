33c2e3ada6e94b12f37b4a9ca09c5cf06030548e
honki12345 <iui4793@gmail.com>
2023-08-06 22:48:03 +0900

    Teat #1 - 다른 핸들러 side effect 때문에 @WebMvcTest 속성에 PostController 추가

--- src/test/java/kr/megaptera/assignment/controllers/PostControllerTest.java
+++ src/test/java/kr/megaptera/assignment/controllers/PostControllerTest.java
@@ -23,7 +23,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest
+@WebMvcTest(controllers = {PostController.class})
 class PostControllerTest {
     @Autowired
     MockMvc mockMvc;
