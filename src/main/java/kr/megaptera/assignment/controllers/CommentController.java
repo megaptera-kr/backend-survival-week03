34982a457abe3455db92a7280b42bcfda8051805
honki12345 <iui4793@gmail.com>
2023-08-06 23:24:32 +0900

    Feat #4 - 댓글 생성 api 구현

--- src/main/java/kr/megaptera/assignment/controllers/CommentController.java
+++ src/main/java/kr/megaptera/assignment/controllers/CommentController.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,5 +28,13 @@ public class CommentController {
     public ResponseEntity<List<CommentDTO>> list(@RequestParam Long postId) {
         List<CommentDTO> commentDTOs = commentService.list(postId);
         return new ResponseEntity<>(commentDTOs, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<String> create(
+            @RequestParam Long postId,
+            @RequestBody CommentDTO commentDTO) {
+        commentService.create(postId, commentDTO);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 }
