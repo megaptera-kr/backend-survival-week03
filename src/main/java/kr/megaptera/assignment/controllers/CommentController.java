fc5602a1c7dff9e6f6658847cd10197786b8ffa4
honki12345 <iui4793@gmail.com>
2023-08-07 00:24:51 +0900

    Feat #4 - 댓글 수정 api 구현

--- src/main/java/kr/megaptera/assignment/controllers/CommentController.java
+++ src/main/java/kr/megaptera/assignment/controllers/CommentController.java
@@ -6,7 +6,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -36,5 +38,14 @@ public class CommentController {
             @RequestBody CommentDTO commentDTO) {
         commentService.create(postId, commentDTO);
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> update(
+            @PathVariable Long id,
+            @RequestParam Long postId,
+            @RequestBody CommentDTO commentDTO) {
+        commentService.update(id, commentDTO);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
