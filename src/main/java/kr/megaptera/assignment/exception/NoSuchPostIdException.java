b55089ee1d0a20d424d777b4f508a44a7bc5cf38
honki12345 <iui4793@gmail.com>
2023-08-06 18:26:40 +0900

    Feat #3 - PostService 에서 PostId가 없을 시 발생하는 예외객체

---
+++ src/main/java/kr/megaptera/assignment/exception/NoSuchPostIdException.java
@@ -0,0 +1,7 @@
+package kr.megaptera.assignment.exception;
+
+public class NoSuchPostIdException extends RuntimeException{
+    public NoSuchPostIdException() {
+        super("해당 id의 게시글은 존재하지 않습니다");
+    }
+}
