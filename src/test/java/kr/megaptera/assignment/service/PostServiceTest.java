43cc117e778473eaa15d609c663ca737235c4f4e
honki12345 <iui4793@gmail.com>
2023-08-06 22:05:50 +0900

    Teat #3 - 게시글 수정 통합테스트 리팩토링 및 게시글 삭제 통합테스트 추가
    게시글을 저장한 후 반환값으로 나온 게시글에서 id 값을 변수로 저장하였다. 그리고 수정하는 게시글의 id 값이 변수를 사용하였다.
    
    통합테스트에 수정과 더불어 삭제까지 추가하니까 테스트 순서에 따라 문제가 발생하였다. 테스트 간에 의존성이 발생하지 않고, 하나의 테스트 안에 여러 요소의 테스트가 들어가지 않도록 하기 위해 리팩토링하였다.

--- src/test/java/kr/megaptera/assignment/service/PostServiceTest.java
+++ src/test/java/kr/megaptera/assignment/service/PostServiceTest.java
@@ -2,13 +2,14 @@ package kr.megaptera.assignment.service;
 
 import kr.megaptera.assignment.domain.Post;
 import kr.megaptera.assignment.dtos.PostDTO;
+import kr.megaptera.assignment.exception.NoSuchPostIdException;
 import kr.megaptera.assignment.repository.PostRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -19,24 +20,41 @@ class PostServiceTest {
     @Autowired
     PostService postService;
 
-    @DisplayName("")
+    @DisplayName("게시글을 수정한다")
     @Test
-    void test() {
+    void update() {
         // given
         Post post = Post.builder()
                 .title("title1")
                 .content("content1")
                 .build();
-        postRepository.save(post);
+        Post savedPost = postRepository.save(post);
 
         // when
-        postService.update(PostDTO.of("title2", "content2"), 1L);
+        postService.update(PostDTO.of("title2", "content2"), savedPost.getId());
 
         // then
-        PostDTO actual = postService.get(1L);
-        Assertions.assertThat(actual)
-                .hasFieldOrPropertyWithValue("title", "title2");
+        PostDTO actual = postService.get(savedPost.getId());
+        assertThat(actual)
+                .hasFieldOrPropertyWithValue("title", "title2")
+                .hasFieldOrPropertyWithValue("content", "content2");
 
     }
 
+    @DisplayName("게시글을 삭제한다")
+    @Test
+    void delete() throws Exception {
+        // Given
+        Post post = Post.builder()
+                .title("title1")
+                .content("content1")
+                .build();
+        Post savedPost = postRepository.save(post);
+
+        // When
+        postService.delete(1L);
+
+        // Then
+        assertThrows(NoSuchPostIdException.class, () -> postService.get(savedPost.getId()));
+    }
 }
