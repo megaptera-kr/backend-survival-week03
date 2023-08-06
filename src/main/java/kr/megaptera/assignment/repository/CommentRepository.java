f3e621386d804b9ccc2af54ae9d035ce4af46ef3
honki12345 <iui4793@gmail.com>
2023-08-07 00:26:29 +0900

    Feat #3 - findByPostId() 반환값 Optional 로 바꾸어 예외처리

--- src/main/java/kr/megaptera/assignment/repository/CommentRepository.java
+++ src/main/java/kr/megaptera/assignment/repository/CommentRepository.java
@@ -4,7 +4,8 @@ import kr.megaptera.assignment.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    List<Comment> findByPostId(Long postId);
+    Optional<List<Comment>> findByPostId(Long postId);
 }
