f390fdcfed6a9abce3acd9ad88d62c312a47b4fa
honki12345 <iui4793@gmail.com>
2023-08-06 22:46:47 +0900

    Feat #3 - CommentRepository postId 로 검색하는 메소드 추가

--- src/main/java/kr/megaptera/assignment/repository/CommentRepository.java
+++ src/main/java/kr/megaptera/assignment/repository/CommentRepository.java
@@ -3,5 +3,8 @@ package kr.megaptera.assignment.repository;
 import kr.megaptera.assignment.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByPostId(Long postId);
 }
