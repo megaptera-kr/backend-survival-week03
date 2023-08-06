0d4cba8ad8cd3c8f592e62ed7b3d1985c7dfcedd
honki12345 <iui4793@gmail.com>
2023-08-06 21:09:23 +0900

    Feat #3 - 게시글 수정 update() 추가

--- src/main/java/kr/megaptera/assignment/service/PostService.java
+++ src/main/java/kr/megaptera/assignment/service/PostService.java
@@ -6,10 +6,12 @@ import kr.megaptera.assignment.exception.NoSuchPostIdException;
 import kr.megaptera.assignment.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Transactional
 @Service
 @RequiredArgsConstructor
 public class PostService {
@@ -39,5 +41,10 @@ public class PostService {
     public PostDTO get(Long id) {
         Post post = postRepository.findById(id).orElseThrow(NoSuchPostIdException::new);
         return new PostDTO(post.getId(), post.getTitle(), post.getContent());
+    }
+
+    public void update(PostDTO postDTO, Long id) {
+        Post post = postRepository.findById(id).orElseThrow(NoSuchPostIdException::new);
+        post.update(postDTO.title(), postDTO.content());
     }
 }
