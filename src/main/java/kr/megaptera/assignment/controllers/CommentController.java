0180e7519a0aa74841ac1463b4d3bb407205b817
honki12345 <iui4793@gmail.com>
2023-08-06 22:45:42 +0900

    Feat #4 - 댓글 조회 api 구현

--- src/main/java/kr/megaptera/assignment/controllers/CommentController.java
+++ src/main/java/kr/megaptera/assignment/controllers/CommentController.java
@@ -1,12 +1,30 @@
 package kr.megaptera.assignment.controllers;
 
-import kr.megaptera.assignment.dtos.CommentDto;
+import kr.megaptera.assignment.dtos.CommentDTO;
+import kr.megaptera.assignment.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@RestController
+@RequestMapping("/comments")
+@RequiredArgsConstructor
 public class CommentController {
+    private final CommentService commentService;
     private Long newId = 0L;
 
-    private List<CommentDto> commentDtos = new ArrayList<>();
+    private List<CommentDTO> commentDtos = new ArrayList<>();
+
+    @GetMapping
+    public ResponseEntity<List<CommentDTO>> list(@RequestParam Long postId) {
+        List<CommentDTO> commentDTOs = commentService.list(postId);
+        return new ResponseEntity<>(commentDTOs, HttpStatus.OK);
+    }
 }
