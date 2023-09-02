77e835e287210d056fba47f715d9d06be6fd8b88
DESKTOP-LH02614\jhp52 <jhp5234@gmail.com>
2023-09-02 21:31:12 +0900

    게시글 컨트롤러 구현

--- src/main/java/kr/megaptera/assignment/controllers/PostController.java
+++ src/main/java/kr/megaptera/assignment/controllers/PostController.java
@@ -1,12 +1,76 @@
 package kr.megaptera.assignment.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import kr.megaptera.assignment.dtos.PostDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@RestController
+@RequestMapping("/posts")
 public class PostController {
     private Long newId = 0L;
-
     private List<PostDto> postDtos = new ArrayList<>();
+    private final ObjectMapper objectMapper;
+
+    public PostController(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    @GetMapping
+    public List<PostDto> list() {
+        return postDtos;
+    }
+
+    @GetMapping("/{id}")
+    public PostDto detail(@PathVariable String id) {
+        for (int i = 0; i < postDtos.size(); i++) {
+            PostDto postDto = postDtos.get(i);
+            if (postDto.getId().equals(id)) {
+                return postDto;
+            }
+        }
+        return null;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String create(@RequestBody PostDto postDto) {
+        postDtos.add(postDto);
+        return "Complete!";
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update(@PathVariable String id, @RequestBody PostDto newPostDto) {
+        for (int i = 0; i < postDtos.size(); i++) {
+            PostDto postDto = postDtos.get(i);
+            if (postDto.getId().equals(id)) {
+                postDtos.set(i, newPostDto);
+                return;
+            }
+        }
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable String id) {
+        for (int i = 0; i < postDtos.size(); i++) {
+            PostDto postDto = postDtos.get(i);
+            if (postDto.getId().equals(id)) {
+                postDtos.remove(i);
+                return;
+            }
+        }
+    }
 }
