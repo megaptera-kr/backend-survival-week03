54f77c6cd750d26056f1b5e0be75fc0b2bc653a9
DongJae <tlsehdwo7@naver.com>
2023-10-09 16:44:10 +0900

    feat : 2023-10-09 메가테라 3주차 백엔드 DTO 및 CORS 보안 문제 해결

--- src/main/java/kr/megaptera/assignment/controllers/CommentController.java
+++ src/main/java/kr/megaptera/assignment/controllers/CommentController.java
@@ -1,12 +1,70 @@
 package kr.megaptera.assignment.controllers;
 
 import kr.megaptera.assignment.dtos.CommentDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
+@RestController
+@RequestMapping("/comments")
 public class CommentController {
     private Long newId = 0L;
 
     private List<CommentDto> commentDtos = new ArrayList<>();
+
+    @GetMapping
+    public List<CommentDto> list(@RequestParam String postId) {
+        List<CommentDto> commentDtoList = commentDtos.stream()
+                .filter(i -> i.id().equals(postId))
+                .toList();
+
+        return commentDtoList;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String create(@RequestParam String postId, @RequestBody CommentDto commentDto
+    ) {
+        CommentDto AddedIdCommentDto = new CommentDto(createId(),postId, commentDto.content());
+        commentDtos.add(AddedIdCommentDto);
+
+        return "Complete!";
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update(
+            @PathVariable String id,
+            @RequestParam String postId,
+            @RequestBody CommentDto commentDto
+    ) {
+        commentDtos = commentDtos.stream()
+                .map(i -> i.postId().equals(postId) && i.id().equals(id)
+                        ? commentDto
+                        : i)
+                .collect(Collectors.toList());
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(
+            @RequestParam String postId,
+            @PathVariable String id
+    ) {
+        CommentDto commentDto = commentDtos.stream()
+                .filter(i -> i.postId().equals(postId) && i.id().equals(id))
+                .findFirst()
+                .get();
+
+        commentDtos.remove(commentDto);
+    }
+
+    //정적 팩토리 메서드 구현
+    private String createId() {
+        newId++;
+        return newId.toString();
+    }
 }
--- src/main/java/kr/megaptera/assignment/controllers/PostController.java
+++ src/main/java/kr/megaptera/assignment/controllers/PostController.java
@@ -1,12 +1,57 @@
 package kr.megaptera.assignment.controllers;
 
 import kr.megaptera.assignment.dtos.PostDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
+@RestController
+@RequestMapping("/posts")
 public class PostController {
     private Long newId = 0L;
-
     private List<PostDto> postDtos = new ArrayList<>();
+
+    @GetMapping
+    public List<PostDto> listPost() {
+        return postDtos.stream().toList();
+    }
+
+    @GetMapping("/{id}")
+    public PostDto getPost(@PathVariable String id) {
+        return postDtos.stream().filter(item -> item.id().equals(id)).findFirst().orElse(null);
+
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String createPost(@RequestBody PostDto postRequestDto) {
+        PostDto AddedIdPostDto = new PostDto(createId(),postRequestDto.title(),postRequestDto.content());
+        postDtos.add(AddedIdPostDto);
+        return "Complete!";
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void putPosts(@PathVariable String id, @RequestBody PostDto postRequestDto) {
+        postDtos = postDtos.stream().map(item -> item.id().equals(id) ? postRequestDto : item)
+                .collect(Collectors.toList());
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletePost(@PathVariable String id) {
+        PostDto postDto = postDtos.stream().filter(item -> item.id().equals(id)).findFirst().orElse(null);
+        if (postDto != null) {
+            postDtos.remove(postDto);
+        }
+    }
+
+    //정적 팩토리 메서드 구현
+    private String createId() {
+        newId++;
+        return newId.toString();
+    }
 }
