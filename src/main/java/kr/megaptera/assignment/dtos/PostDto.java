49d8c05fc53ef36b7221bcf1b83ee2cdf29f071d
gogoheejun <heejjuunn@gmail.com>
2023-12-26 21:09:48 +0900

    post 구현

--- src/main/java/kr/megaptera/assignment/dtos/PostDto.java
+++ src/main/java/kr/megaptera/assignment/dtos/PostDto.java
@@ -1,4 +1,41 @@
 package kr.megaptera.assignment.dtos;
 
 public class PostDto {
+
+    String id;
+    String title;
+    String content;
+
+    public PostDto() {
+    }
+
+    public PostDto(String id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
