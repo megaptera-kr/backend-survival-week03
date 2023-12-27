f7676b51a6fdd52f360996ae0fc4437b2ad8be86
goeo <goeo1066@gmail.com>
2023-12-27 17:55:05 +0900

    [백엔드 9기 어경오] 3주차 과제 - DTO를 이용한 REST API 만들기

--- src/main/java/kr/megaptera/assignment/controllers/CommentController.java
+++ src/main/java/kr/megaptera/assignment/controllers/CommentController.java
@@ -1,12 +1,62 @@
 package kr.megaptera.assignment.controllers;
 
 import kr.megaptera.assignment.dtos.CommentDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@RestController
+@RequestMapping("/comments")
 public class CommentController {
     private Long newId = 0L;
-
     private List<CommentDto> commentDtos = new ArrayList<>();
+
+    private Long getNextId() {
+        return newId++;
+    }
+
+    @GetMapping
+    private List<CommentDto> list(@RequestParam String postId) {
+        List<CommentDto> commentDtoList = new ArrayList<>();
+        for (CommentDto commentDto : commentDtos) {
+            if (commentDto.getPostId().equals(postId)) {
+                commentDtoList.add(commentDto);
+            }
+        }
+        return commentDtoList;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    private String create(@RequestParam String postId, @RequestBody CommentDto body) {
+        body.setId("" + getNextId());
+        body.setPostId(postId);
+        commentDtos.add(body);
+        return "Complete!";
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    private void update(@PathVariable String id, @RequestParam String postId, @RequestBody CommentDto body) {
+        for (int i = 0; i < commentDtos.size(); i++) {
+            CommentDto commentDto = commentDtos.get(i);
+            if (commentDto.getId().equals(id) && commentDto.getPostId().equals(postId)) {
+                commentDtos.set(i, body);
+            }
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    private void delete(@PathVariable String id, @RequestParam String postId) {
+        for (int i = 0; i < commentDtos.size(); i++) {
+            CommentDto commentDto = commentDtos.get(i);
+            if (commentDto.getId().equals(id) && commentDto.getPostId().equals(postId)) {
+                commentDtos.remove(i);
+                break;
+            }
+        }
+    }
 }
--- src/main/java/kr/megaptera/assignment/dtos/CommentDto.java
+++ src/main/java/kr/megaptera/assignment/dtos/CommentDto.java
@@ -1,4 +1,55 @@
 package kr.megaptera.assignment.dtos;
 
+import java.util.Objects;
+
 public class CommentDto {
+    private String id;
+    private String postId;
+    private String content;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPostId() {
+        return postId;
+    }
+
+    public void setPostId(String postId) {
+        this.postId = postId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CommentDto that = (CommentDto) o;
+        return Objects.equals(getId(), that.getId()) && Objects.equals(getPostId(), that.getPostId()) && Objects.equals(getContent(), that.getContent());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getPostId(), getContent());
+    }
+
+    @Override
+    public String toString() {
+        return "CommentDto{" +
+                "id='" + id + '\'' +
+                ", postId='" + postId + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
 }
