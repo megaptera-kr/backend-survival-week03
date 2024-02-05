f7d732948031ff480d66b00ecf6027d02e49b91d
Jeongpope <nullptr-@kakao.com>
2024-02-05 16:06:41 +0900

    update dtos

--- src/main/java/kr/megaptera/assignment/dtos/PostDto.java
+++ src/main/java/kr/megaptera/assignment/dtos/PostDto.java
@@ -1,4 +1,40 @@
 package kr.megaptera.assignment.dtos;
 
+import java.util.List;
+
 public class PostDto {
+
+    private Long id;
+    private String title;
+    private String content;
+
+    public PostDto(Long id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
