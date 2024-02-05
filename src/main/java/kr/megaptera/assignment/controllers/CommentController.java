a9c87779ef27c3befd7f5a7c55056327de612856
Jeongpope <nullptr-@kakao.com>
2024-02-05 16:06:49 +0900

    update apis

--- src/main/java/kr/megaptera/assignment/controllers/CommentController.java
+++ src/main/java/kr/megaptera/assignment/controllers/CommentController.java
@@ -1,12 +1,72 @@
 package kr.megaptera.assignment.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import kr.megaptera.assignment.dtos.CommentDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@RestController
+@RequestMapping("/comments")
 public class CommentController {
     private Long newId = 0L;
 
     private List<CommentDto> commentDtos = new ArrayList<>();
+
+    @GetMapping()
+    public List<CommentDto> getComments(@RequestParam Long postId) {
+        return this.commentDtos.stream()
+                .filter(commentDto -> commentDto.getPostId().equals(postId))
+                .toList();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String createComment(@RequestParam String postId, @RequestBody CommentDto commentDto) throws Exception {
+        commentDto.setId(++newId);
+        commentDto.setPostId(Long.valueOf(postId));
+        this.commentDtos.add(commentDto);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(commentDtos);
+
+        System.out.println(json);
+
+        return "Complete!";
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateComment(@PathVariable Long id, @RequestParam Long postId, @RequestBody CommentDto commentDto) {
+        CommentDto targetCommentDto = this.commentDtos.stream()
+                .filter(c -> c.getId().equals(id) && c.getPostId().equals(postId))
+                .findFirst()
+                .orElse(null);
+
+        if (targetCommentDto == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Not found");
+        }
+
+        targetCommentDto.setContent(commentDto.getContent());
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteComment(@PathVariable Long id, @RequestParam Long postId) {
+        CommentDto targetCommentDto = this.commentDtos.stream()
+                .filter(c -> c.getId().equals(id) && c.getPostId().equals(postId))
+                .findFirst()
+                .orElse(null);
+
+        if (targetCommentDto == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Not found");
+        }
+
+        this.commentDtos.remove(targetCommentDto);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
+    }
 }
--- src/main/java/kr/megaptera/assignment/controllers/PostController.java
+++ src/main/java/kr/megaptera/assignment/controllers/PostController.java
@@ -1,12 +1,72 @@
 package kr.megaptera.assignment.controllers;
 
 import kr.megaptera.assignment.dtos.PostDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@RestController
+@RequestMapping("/posts")
 public class PostController {
     private Long newId = 0L;
 
     private List<PostDto> postDtos = new ArrayList<>();
+
+    @GetMapping
+    public List<PostDto> getPosts() {
+        return this.postDtos;
+    }
+
+    @GetMapping("/{id}")
+    public PostDto getPost(@PathVariable Long id) {
+        return this.postDtos.stream()
+                .filter(postDto -> postDto.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String createPost(@RequestBody PostDto postDto) {
+        postDto.setId(++newId);
+        this.postDtos.add(postDto);
+
+        return "Complete!";
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updatePost(@PathVariable Long id, @RequestBody PostDto postDto) {
+        PostDto targetPostDto = this.postDtos.stream()
+                .filter(p -> p.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+
+        if (targetPostDto == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Not found");
+        }
+
+        targetPostDto.setTitle(postDto.getTitle());
+        targetPostDto.setContent(postDto.getContent());
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deletePost(@PathVariable Long id) {
+        PostDto targetPostDto = this.postDtos.stream()
+                .filter(p -> p.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+
+        if (targetPostDto == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Not found");
+        }
+
+        this.postDtos.remove(targetPostDto);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
+    }
 }
