b1409c2d7421e1d74967f0d8af8d572f4806b64c
백철현 <bch410953@gmail.com>
2024-02-20 04:09:22 +0900

    3주차과제

--- src/main/java/kr/megaptera/assignment/dtos/PostDto.java
+++ src/main/java/kr/megaptera/assignment/dtos/PostDto.java
@@ -1,4 +1,40 @@
 package kr.megaptera.assignment.dtos;
 
 public class PostDto {
+    private String title;
+    private String content;
+    private String id;
+
+    public PostDto() {
+    }
+
+    public PostDto(String title, String content, String id) {
+        this.title = title;
+        this.content = content;
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 }
