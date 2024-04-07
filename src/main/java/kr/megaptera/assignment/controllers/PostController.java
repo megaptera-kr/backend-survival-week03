d334d4f65161623fdc0200e7e5a7700fb8df3dd6
Kimyerim1935 <kimyerim1935@naver.com>
2024-04-07 19:54:51 +0900

    add: 3주차 과제

--- src/main/java/kr/megaptera/assignment/controllers/PostController.java
+++ src/main/java/kr/megaptera/assignment/controllers/PostController.java
@@ -1,12 +1,77 @@
 package kr.megaptera.assignment.controllers;
 
+import com.fasterxml.jackson.core.JacksonException;
+import jakarta.servlet.http.HttpServletResponse;
 import kr.megaptera.assignment.dtos.PostDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
+@RestController
+@RequestMapping("/posts")
 public class PostController {
     private Long newId = 0L;
 
     private List<PostDto> postDtos = new ArrayList<>();
+
+    @GetMapping
+    public List<PostDto> list() {
+        return postDtos;
+    }
+    @GetMapping("/{id}")
+    public PostDto listDetail(@PathVariable String id){
+        PostDto postDto = postDtos.stream()
+                .filter(i -> i.getId().equals(id))
+                .findFirst()
+                .get();
+
+        return postDto;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String create(@RequestBody PostDto postDto) {
+        postDto.setId(generateId());
+        postDtos.add(postDto);
+
+        return "Complete!";
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public PostDto update(@PathVariable String id,  @RequestBody PostDto postDto) {
+        postDtos = postDtos.stream()
+                .map(i -> i.getId().equals(id) ? postDto : i)
+                .collect(Collectors.toList());
+        return postDto;
+    }
+
+    private String generateId() {
+        newId += 1;
+
+        return newId.toString();
+    }
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable String id) {
+        PostDto postDto = postDtos.stream()
+                .filter(i -> i.getId().equals(id))
+                .findFirst()
+                .get();
+
+        postDtos.remove(postDto);
+    }
+
 }
